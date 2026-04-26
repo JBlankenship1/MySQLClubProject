5bbc1580263d51a0ee5114d747ab56abf55698ad
elliepone <elizabeth.morey@uky.edu>
2026-04-26 07:17:31 -0400

    added trigger to prevent double booking classrooms, set all attributes to NOT NULL if not primary key and set all constraints to on update cascade

--- Database.sql
+++ Database.sql
@@ -19,23 +19,23 @@ Create tables
 --First create student table, no foreign keys
 CREATE TABLE Students (
 studentID char(6) primary key,
-studentName varchar(20)
+studentName varchar(20) NOT NULL
 );
 
 --Second create teacher table, no foreign keys
 CREATE TABLE Faculty(
 facultyID char(6) primary key,
-facultyName varchar(20)
+facultyName varchar(20) NOT NULL
 );
 
 --Third create clubs table, which needs teachers as foreign keys
 CREATE TABLE Clubs(
 clubName VARCHAR (20),
 clubYear INT CHECK (clubYear > 1900 AND clubYear < 2100),
-budget DECIMAL (10,2),
-facultyID CHAR (6),
+budget DECIMAL (10,2) NOT NULL,
+facultyID CHAR (6) NOT NULL,
 PRIMARY KEY (clubName,clubYear),
-CONSTRAINT faculty_clubs foreign key (facultyID) references Faculty(facultyID) on delete CASCADE
+CONSTRAINT faculty_clubs foreign key (facultyID) references Faculty(facultyID) on UPDATE CASCADE
 );
 
 --Fourth create Are_in table
@@ -44,42 +44,67 @@ studentID char(6),
 clubName varchar (20),
 clubYear INT,
 PRIMARY KEY (studentID, clubName, clubYear),
-constraint clubs_arein foreign key (clubName, clubYear) references Clubs(clubName, clubYear) ON DELETE CASCADE,
-constraint students_arein foreign key (studentID) REFERENCES Students(studentID)
+constraint clubs_arein foreign key (clubName, clubYear) references Clubs(clubName, clubYear) ON UPDATE CASCADE,
+constraint students_arein foreign key (studentID) REFERENCES Students(studentID) ON UPDATE CASCADE
 );
 
 --Fifth Expenses
 CREATE TABLE Expenses(
 expenseID CHAR(10) PRIMARY KEY,
-expenseDATE DATE,
-amount DECIMAL (10,2),
-expenseDescription VARCHAR (30),
-clubName VARCHAR (20),
-clubYear INT,
-CONSTRAINT Exp_Club FOREIGN KEY (clubName, clubYear) REFERENCES Clubs(clubName, clubYear)
+expenseDATE DATE NOT NULL,
+amount DECIMAL (10,2) NOT NULL,
+expenseDescription VARCHAR (30) NOT NULL,
+clubName VARCHAR (20) NOT NULL,
+clubYear INT NOT NULL,
+CONSTRAINT Exp_Club FOREIGN KEY (clubName, clubYear) REFERENCES Clubs(clubName, clubYear) ON UPDATE CASCADE
 );
 
 --Sixth Meetings
 CREATE TABLE Meetings(
 meetingDate DATE,
-classroom VARCHAR (20),
-meetingStartTime TIME,
-meetingEndTime TIME,
-meetingDescription VARCHAR (30),
+classroom VARCHAR (20) NOT NULL,
+meetingStartTime TIME NOT NULL,
+meetingEndTime TIME NOT NULL,
+meetingDescription VARCHAR (30) NOT NULL,
 clubName VARCHAR (20),
 clubYear INT,
 PRIMARY KEY (meetingDATE,clubName, clubYear),
-CONSTRAINT meet_club FOREIGN KEY (clubName, clubYear) references Clubs (clubName, clubYear)
+CONSTRAINT meet_club FOREIGN KEY (clubName, clubYear) references Clubs (clubName, clubYear) ON UPDATE CASCADE
 );
 
 --Seventh Events
 CREATE TABLE Events (
     eventDate DATE,
-    eventStartTime TIME,
-    eventEndTime TIME,
-    eventDescription VARCHAR(30),
+    eventStartTime TIME NOT NULL,
+    eventEndTime TIME NOT NULL,
+    eventDescription VARCHAR(30) NOT NULL,
     clubName VARCHAR (20),
     clubYear INT,
     PRIMARY KEY (eventDate,clubName,clubYear),
-    CONSTRAINT event_club FOREIGN KEY (clubName, clubYear) REFERENCES Clubs (clubName,clubYear)
+    CONSTRAINT event_club FOREIGN KEY (clubName, clubYear) REFERENCES Clubs (clubName,clubYear) ON UPDATE CASCADE
 );
+ 
+ --prevent double booking meetings in same classroom
+
+DELIMITER //
+
+CREATE TRIGGER check_classroom
+BEFORE INSERT ON Meetings
+FOR EACH ROW
+BEGIN
+    IF EXISTS (
+        SELECT *
+        FROM Meetings
+        WHERE classroom = NEW.classroom
+          AND meetingDate = NEW.meetingDate
+          AND NOT (
+              NEW.meetingEndTime <= meetingStartTime
+              OR NEW.meetingStartTime >= meetingEndTime
+          )
+    ) THEN
+        SIGNAL SQLSTATE '45000'
+        SET MESSAGE_TEXT = 'Classroom is booked during that time';
+    END IF;
+END//
+
+DELIMITER ;
